1b3a489033ebacae57bb06df9496d1745500637f
brian d foy <brian.d.foy@gmail.com>
2017-02-16 06:09:49 -0500

    Added the latest Perl jobs posts

--- get_jobs.pl
+++ get_jobs.pl
@@ -17,7 +17,13 @@ say "$count, $first, $last";
 my $dir = 'perl.jobs';
 mkdir $dir, 0755 unless -d $dir;
 
+my %skips = map { $_, undef } qw(
+	2079
+	3338
+	);
+
 foreach my $i ( $first .. $last ) {
+	next if exists $skips{$i};
 	my $file = catfile( $dir, "$i.txt" );
 	next if -s $file;
 	open my $fh, '>', $file;
@@ -30,10 +36,11 @@ foreach my $i ( $first .. $last ) {
 $nntp->quit;
 
 if( $ENV{PERL_JOBS_COMMIT} ) {
-	system "git add perl.jobs";
-	system "git commit -a -m 'Added the latest Perl jobs posts'";
-	system "git push";
-	}          
+	system "git add --quiet perl.jobs";
+	system "git commit --quiet -a -m 'Added the latest Perl jobs posts'";
+	system "git push --quiet";
+	}
+
 =head1 NAME
 
 =head1 SYNOPSIS
